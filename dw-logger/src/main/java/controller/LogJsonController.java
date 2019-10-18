dc0f65ac2c2cf3bd08b59b3a033e6d4afd6d3d15
WangPeng <18391447833@163.com>
2019-10-18 17:09:57 +0800

    首次提交程序框架

---
+++ dw-logger/src/main/java/controller/LogJsonController.java
@@ -0,0 +1,9 @@
+package controller;
+@Controller
+public class LogJsonController {
+
+
+    public String testDemo(){
+        return "hello demo";
+    }
+}
---
+++ dw-logger/src/test/java/com/atguigu/gmall/dw/dwlogger/DwLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.dw.dwlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
